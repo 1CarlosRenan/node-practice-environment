7bc19c7c846bf4d864d3341fcafb7c640f637c52
1CarlosRenan <carlosrenan96@gmail.com>
2022-11-21 23:42:47 -0300

    exploring console

---
+++ course/files/2-8-exploring-console/index.js
@@ -0,0 +1,10 @@
+x = 10
+z = 'carlos'
+
+console.count(`O valor de x é: ${x}, contagem`)
+console.count(`O valor de x é: ${x}, contagem`)
+console.count(`O valor de x é: ${x}, contagem`)
+
+console.log('O nome é %s, ele é programador', z)
+
+console.clear()
