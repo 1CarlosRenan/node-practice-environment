9feb031ee2fc030151f19b22e9753cfc208ecd36
1CarlosRenan <carlosrenan96@gmail.com>
2022-11-22 23:14:13 -0300

    first app

---
+++ express/express-node-basic/src/index.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const app = express()
+
+app.get('/', function (request, response) {
+  return response.json({
+    message: 'Olá Dev! Bem vindo'
+  })
+})
+
+app.listen(3000, () => {
+  console.log('Server starterd on port 3000!')
+})
