1f7d867f0ce9db5ee854f3ca1b34b21802396eae
1CarlosRenan <carlosrenan96@gmail.com>
2022-11-22 23:25:15 -0300

    using nodemon

--- express/express-node-basic/src/index.js
+++ express/express-node-basic/src/index.js
@@ -3,10 +3,10 @@ const app = express()
 
 app.get('/', function (request, response) {
   return response.json({
-    message: 'Olá Dev! Bem vindo'
+    message: 'Olá Dev! Bem vindo!'
   })
 })
 
 app.listen(3000, () => {
-  console.log('Server starterd on port 3000!')
+  console.log('Server starterd on port 3000.')
 })
